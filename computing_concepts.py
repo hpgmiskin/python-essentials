3d1df3f937968c85660ecc0783bb00e6e45dff51
Henry Miskin <henry.miskin@talktalk.net>
2015-11-29 13:49:18 +0000

    Added computing concepts

---
+++ computing_concepts.py
@@ -0,0 +1,41 @@
+import utilities
+# Basic computing concepts
+
+# Itteration
+# ======================================================================
+utilities.printHeading('Itteration')
+
+# For loop
+for i in range(10):
+    print(i)
+
+
+# While loop
+index = 0
+while (index < 10):
+    print(index)
+    index = index + 2
+
+# Selection
+# ======================================================================
+utilities.printHeading('Selection')
+
+# If statement
+if True:
+    print('True is True')
+else:
+    print('True is not True')
+
+# Boolean logic
+print('10 > 9 is',(10 > 9))
+print('2 == 2 is',(2 == 2))
+print('2 == 3 is',(2 == 3))
+
+# Boolean logic in if statement
+ten = 10
+if (ten > 9):
+    print('{} > 9'.format(ten))
+
+
+
+
