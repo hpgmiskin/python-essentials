ba7925970fe2e176820f8f11f9f4ba2c3607b4ff
Henry Miskin <henry.miskin@talktalk.net>
2015-11-29 13:12:44 +0000

    Added basic data structures

---
+++ basic_data_structures.py
@@ -0,0 +1,22 @@
+# THis is an introduction to basic computing concetps
+
+# Here is are some variables (they are being assigned)
+myInt = 10 
+myFloat = 1.2
+myString = 'hello'
+
+# Variables can be printed
+print(myString)
+
+# Variable types can be printed
+print(type(myInt),type(myFloat),type(myString))
+
+# Variabels can be added
+print(myInt + myFloat)
+
+# Strings can be multiplied
+print(myString * myInt)
+
+# Variables can be edited
+myFloat = myFloat + 10
+print(myFloat)
